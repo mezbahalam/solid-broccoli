cd8e4a74d8dfc7a8c4b13f6ac1c7d37c22a9e9d6
mezbahalam <mezbahalam26@gmail.com>
2019-05-31 03:53:53 +0600

    initial commit 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190530200403) do
+ActiveRecord::Schema.define(version: 20190530200153) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"
